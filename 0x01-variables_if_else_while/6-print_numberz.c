42ae6b66c025b762d6de59b05dd3afd7d9feb082
Admasu <admasud89@gmail.com>
2022-09-11 14:17:33 -0700

    display numbers from 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,32 +1,16 @@
 #include <stdio.h>
-
 /**
-
 * main - Print number from 0 to 9
-
 * description - in the main function
-
-*
-
 * Return: 0
-
 */
 int main(void)
-
 {
-
 int n;
-
 for (n = '0'; n <= '9'; n++)
-
 {
-
 putchar(n);
-
 }
-
 putchar('\n');
-
 return (0);
-
 }
