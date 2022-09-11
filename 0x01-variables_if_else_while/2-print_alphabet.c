b4437a49a06efc326abd696a77030d6dbf6767b2
Admasu <admasud89@gmail.com>
2022-09-11 12:47:07 -0700

    use putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - lowercase alphabets
+* description - in the main function
+* Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
