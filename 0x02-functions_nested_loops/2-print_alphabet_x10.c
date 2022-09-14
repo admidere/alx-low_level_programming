24bf7e4438a1be94d3e1f2879f510d632dc7184b
admidere <admasud89@gmail.com>
2022-09-14 16:10:56 +0300

    Write a function that prints the alphabet, in lowercase,

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
- * main - prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *
  * return: 0
- */
+*/
+ 
 void print_alphabet_x10(void)
 {
 	char c, t;
+
 	for (t = 0; t < 10; t++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
@@ -16,4 +19,3 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
-
