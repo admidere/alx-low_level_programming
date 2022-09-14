7fd46d47c2a021a5d0afda9ce8cb001304853a2d
admidere <admasud89@gmail.com>
2022-09-14 22:12:46 +0300

    sum of the number that are multiple of either 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - print the sum of multiple of 3 and 5
+* return: 0
+*/
+int main(void)
+{
+int n, sum = 0;
+for (n = 0; n < 1024; n++)
+{
+if ((n % 3) == 0 || (n % 5) == 0)
+sum = sum + n;
+}
+printf("%d\n", sum);
+return (0);
+}
