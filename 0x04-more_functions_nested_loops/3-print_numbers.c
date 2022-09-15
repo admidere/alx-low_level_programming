cda99c520cdd4dbe8759c7beed4c69b1a4f3760b
admidere <admasud89@gmail.com>
2022-09-15 23:19:54 +0300

    print numbers from o to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,12 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
 *print_numbers - function
-*prints a numbers from 0 to 9
+*print numbers from 0 to 9
 *Return: nothing
 */
+
 void print_numbers(void)
 {
 char c;
