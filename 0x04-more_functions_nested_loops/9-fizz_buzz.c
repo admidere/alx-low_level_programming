a705fe2a9146f33ff06081a66a361a8d7260a8d6
admidere <admasud89@gmail.com>
2022-09-16 00:40:31 +0300

    program that print either number

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
 
 /**
-*main - function
+*main - program that prints either number
 *Return: 0
 */
 
 int main(void)
 {
-int n = 1;
+	int n = 1;
 
-while (n++ < 100)
-{
-if ((n % 3 == 0) && (n % 5 == 0))
-{
-printf("FizzBuzz ");
-}
-else if ((n % 3) == 0)
-{
-printf("Fizz ");
-}
-else if ((n % 5) == 0)
-{
-if (n != 100)
-{
-printf("Buzz ");
-}
-else
-{
-printf("Buzz");
-}
-}
-else
-{
-printf("%d ", n);
-}
-}
-printf("\n");
+	while (n++ < 100)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			if (n != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
 
-return (0);
+	return (0);
 }
