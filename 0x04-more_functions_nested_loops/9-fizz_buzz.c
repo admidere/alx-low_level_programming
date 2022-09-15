c51752aafb2ceb7acb21ceeccac8bcdad8874326
admidere <admasud89@gmail.com>
2022-09-16 00:26:38 +0300

    print thar are both numbers and characters

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+*main - function
+*Return: 0
+*/
+
+int main(void)
+{
+	int n = 1;
+
+	while (n++ < 100)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			if (n != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
