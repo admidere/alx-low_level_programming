c58df3fa46d1a30d60eec85beb2852737f2e6b57
admidere <admasud89@gmail.com>
2022-09-16 00:05:12 +0300

    prints a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_line - prints a straight line
+*@n: parameter
+*Return:cnothing
+*/
+
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+}
