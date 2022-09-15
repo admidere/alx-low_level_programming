d04c629d0432bbafd61ca68a8d20e6475291ec00
admidere <admasud89@gmail.com>
2022-09-15 23:12:37 +0300

    print numbers from o to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,14 @@
 
 /**
 *print_numbers - function
+*prints a numbers from 0 to 9
 *Return: nothing
 */
 
 void print_numbers(void)
 {
-	char c;
-	
-	for (c = '0'; c <= '9'; c++)
-	_putchar(c);
+char c;
+
+for (c = '0'; c <= '9'; c++)
+putchar(c);
 }
-	
-	
-	
