1826a39045b334a68ff9a0937733c1d7a5c29659
admidere <admasud89@gmail.com>
2022-09-16 00:33:13 +0300

    print both numbers and characters

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,35 +7,35 @@
 
 int main(void)
 {
-	int n = 1;
+int n = 1;
 
-	while (n++ < 100)
-	{
-		if ((n % 3 == 0) && (n % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((n % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((n % 5) == 0)
-		{
-			if (n != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
-		}
-		else
-		{
-			printf("%d ", n);
-		}
-	}
-	printf("\n");
+while (n++ < 100)
+{
+if ((n % 3 == 0) && (n % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if ((n % 3) == 0)
+{
+printf("Fizz ");
+}
+else if ((n % 5) == 0)
+{
+if (n != 100)
+{
+printf("Buzz ");
+}
+else
+{
+printf("Buzz");
+}
+}
+else
+{
+printf("%d ", n);
+}
+}
+printf("\n");
 
-	return (0);
+return (0);
 }
