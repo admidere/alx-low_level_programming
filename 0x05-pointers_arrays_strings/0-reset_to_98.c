aed433fbb21bb244e0b6a365808f15cf8af84fc0
admidere <admasud89@gmail.com>
2022-09-19 16:43:11 +0300

    a pointer to an int as parameter and updates the value it

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 #include <stdio.h>
-                                                                                                                
-/**                                                                                                                                     
-* reset_to_98 - function called by main
-* @n: parameter                                                                                                          
+
+/**
+* reset_to_98 - called function
+* @n: parameter
 */
 
 void reset_to_98(int *n)
 {
-
-*n = 98;
-
+	*n = 98;
 }
