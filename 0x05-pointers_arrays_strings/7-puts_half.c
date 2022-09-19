bbb706df7a4071f5901237310950aefc5e0d9345
admidere <admasud89@gmail.com>
2022-09-20 02:27:33 +0300

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,26 +2,26 @@
 #include <string.h>
 
 /**
- * puts_half - prints half numbers
- * @str: string parameter
- * Return: nothing
- */
+* puts_half - prints half numbers
+* @str: string parameter
+* Return: nothing
+*/
 
 void puts_half(char *str)
 {
-	int i, j, k;
+int i, j, k;
 
-	i = strlen(str);
-	if (i % 2 == 1)
-	j = i / 2 + 1;
+i = strlen(str);
+if (i % 2 == 1)
+j = i / 2 + 1;
 
-	else
-	j = i / 2;
+else
+j = i / 2;
 
-   for (k = j; k < i; k++)
-   {
-	putchar(str[k]);
-   }
+for (k = j; k < i; k++)
+{
+putchar(str[k]);
+}
 
-	putchar('\n');
+putchar('\n');
 }
