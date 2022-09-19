e3b3db532a75289df9c296356df3b5bb407cc9fe
admidere <admasud89@gmail.com>
2022-09-19 21:46:53 +0300

    print half numbers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints reveres character
+ * @str: parameter
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (*(str + c) != '\0')
+	{
+		if (c >=5)
+		{
+		break;
+		}
+			putchar(*(str + c));
+		c++;
+	}
+	putchar(10);
+}
