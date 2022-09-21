8b1d0799a5644e4f24da8efaf5f711d124122f11
admidere <admasud89@gmail.com>
2022-09-21 23:35:53 +0300

    a function that compares two strings.

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp - function
+* @s1: parameter
+* @s2: parameter
+* cmp
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	strcmp(s2, s1);
+	strcmp(s1, s1);
+
+	return (0);	
+}
