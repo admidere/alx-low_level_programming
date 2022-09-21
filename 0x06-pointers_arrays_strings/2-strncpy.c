f906fe11ff11a9ac1ea0d8fe4f735ab02c3d83a7
admidere <admasud89@gmail.com>
2022-09-21 23:16:05 +0300

    a function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+_strncpy - function
+* @dest: destination parameter
+* @src; source parameter
+* returns: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
