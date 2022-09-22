e650aab5623861c4713049dc45c3349f1824a856
admidere <admasud89@gmail.com>
2022-09-22 15:30:46 +0300

    a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all first word lowercase letters 
+ * to uppercase
+ * @str: string to be changed
+ * Return: address tp the string
+ */
+ 
+char *cap_string(char *str)
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if ((str[i] >= 'a' && str[i] <= 'z'))
+str[i] = str[i] - 32;
+continue;
+}
+if (str[i] == ' ')
+{
+++i;
+
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+continue;
+}
+
+}
+else
+{
+if (str[i] >= 'A' && str[i] <= 'Z')
+str[i] = str[i] + 32;
+}
+return (str);
+}
+	
