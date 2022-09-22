0584080086f92f796f5d5fe0a4c6161e24886adf
admidere <admasud89@gmail.com>
2022-09-22 20:59:23 +0300

    fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <string.h>
-
 /**
-* _strncpy - function
-* @dest: destination parameter
-* @src: source parameter
-* @n: n parameter
-* returns: dest
+*  _strncpy - C function that copies a string,
+*@dest: buffer storing the string copy
+*@src:the source string
+*@n:max nummber of byte copied
+*Return: dest
 */
 
 char *_strncpy(char *dest, char *src, int n)
