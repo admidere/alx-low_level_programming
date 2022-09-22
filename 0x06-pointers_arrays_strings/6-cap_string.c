55e8d744b2b200d6986cd11a27b86387a1520de9
admidere <admasud89@gmail.com>
2022-09-22 17:37:42 +0300

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,48 +1,32 @@
 #include "main.h"
 
 /**
- * cap_string - changes all first word lowercase letters 
- * to uppercase
- * @str: string to be changed
- * Return: address tp the string
+ * cap_string - capitalizes all first words in a string
+ * @x: string to capitalize
+ *
+ * Return: address of x
  */
- 
-char *cap_string(char *str)
+char *cap_string(char *x)
 {
-int i, j;
-char x[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-for (i = 0; str[i] != '\0'; i++)
-{
-for (j = 0; x[j] != '\0'; j++)
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-if (i == 0)
-{
-if ((str[i] >= 'a' && str[i] <= 'z'))
-str[i] = str[i] - 32;
-continue;
+	while (*(x + i))
+	{
+		if (*(x + i) >= 'a' && *(x + i) <= 'z')
+		{
+			if (i == 0)
+				*(x + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(x + i - 1))
+						*(x + i) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (x);
 }
-if (str[i] == ' ')
-{
-++i;
-
-if (str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - 32;
-continue;
-}
-
-}
-if (str[i] == x[j])
-{
-    str[i] == x[j];
-	continue;
-}
-else
-{
-if (str[i] >= 'A' && str[i] <= 'Z')
-str[i] = str[i] + 32;
-}
-}
-return (str);
-}
-	
