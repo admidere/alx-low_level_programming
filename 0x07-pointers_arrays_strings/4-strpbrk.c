1cbccb5edfa2c550d25def2c749dd711f658ac85
admidere <admasud89@gmail.com>
2022-09-26 23:22:37 +0300

    a function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,23 +4,23 @@
 *_strpbrk - function that fill the memory block
 *@s: s parameter
 *@accept: parameter
-*Return: string
+*Return: 0
 */
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
-	
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; accept[j]; j++)
-		{
-			if (s[i] == accept[j])
-			break;
-		}
-		if (accept[j])
-			return (s + i);
-		}
-	}
-	return (0);
+int i = 0, j = 0;
+
+for (; *(s + i); i++)
+{
+for (; *(accept); j++)
+{
+if (*(s + i) == *(accept + j))
+break;
+}
+if (*(accept + j))
+return (s + i);
+}
+}
+return (0);
 }
