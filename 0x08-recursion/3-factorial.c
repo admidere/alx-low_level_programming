7612a5884314838fbe57523f04d9be45ea0bdfc7
admidere <admasud89@gmail.com>
2022-09-28 15:42:38 +0300

    print the factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,6 +6,7 @@
 * @n: n parameter
 * Return: the result of factorial.
 */
+
 int factorial(int n)
 {
 if (n == 0)
@@ -14,6 +15,7 @@ if (n == 1)
 return (1);
 if (n < 0)
 return (-1);
+
 else
 return (n * factorial(n -1));
 }
