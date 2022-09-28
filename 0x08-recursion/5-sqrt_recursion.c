068e9462a3ac662f8ebdd296b687d9d0ed433cd4
admidere <admasud89@gmail.com>
2022-09-28 17:44:16 +0300

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+ #include "main.h"
+/**
+* sqrt2 - Makes possible to evaluate 
+* from 1 to n
+* @x: x parameter
+* @y: y parameter
+* Return: result
+*/
+int sqrt2(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y > x)
+		return (-1);
+	return (sqr2(x, y + 1));
+}
+/**
+* _sqrt_recursion - returns the
+* natural square root of 
+* @n: Number Integer
+* Return: On success 1.
+*/
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
