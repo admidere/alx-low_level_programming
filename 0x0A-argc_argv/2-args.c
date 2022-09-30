5cc0260c5a71c96e40badd66a26b5dcb4085f7db
admidere <admasud89@gmail.com>
2022-09-30 20:58:53 +0300

    print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,9 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	for(i = 0; i < argc; i++)
-		printf("argv[%2d]: %s\n", i, argv[i]);
-	
-	return (0);
+int i;
+for(i = 0; i < argc; i++)
+printf("argv[%2d]: %s\n", i, argv[i]);
+
+return (0);
 }
