0dd1a6accee247b70a59d876d73bad1f4c75fbc1
admidere <admasud89@gmail.com>
2022-09-30 20:48:27 +0300

    print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Print the name of the program
+* @argc: contains a total number of arguments
+* @argv: contains all the parameters.
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+
+int i = 0;
+
+if (argc > 0)
+{
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+}
+}
