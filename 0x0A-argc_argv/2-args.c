b6718842bcc4e96af46c820f81169f41068b896a
admidere <admasud89@gmail.com>
2022-09-30 21:17:21 +0300

    print all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* main - Print the name of the program
-* @argc: contains a total number of arguments
-* @argv: contains all the parameters.
-* Return: Always 0 (Success)
+*main - Print the name of the program
+*@argc: contains a total number of arguments
+*@argv: contains all the parameters.
+*Return: Always 0 (Success)
 */
 
 int main(int argc, char *argv[])
 {
 int counter;
 for (counter = 0; counter < argc; counter++)
-printf("%s\n", counter, argv[counter]);
+printf("argv[%2d]: %s\n", counter, argv[counter]);
 
 return (0);
 }
