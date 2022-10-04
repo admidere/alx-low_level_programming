330c489e615328c652256be3d63cb644c04c02d3
admidere <admasud89@gmail.com>
2022-10-04 21:21:37 +0300

    print the string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,24 +3,24 @@
 #include <stdlib.h>
 
 /**
- * create_array - creat an array
- * @size: size of an array
- * @c: string parameter
- * return: array of string
- * if it fails it will print NULL
- */
+* create_array - creat an array
+* @size: size of an array
+* @c: string parameter
+* return: array of string
+* if it fails it will print NULL
+*/
 
 char *create_array(unsigned int size, char c)
 {
-    char *arr;
-    unsigned int i;
-    if (size == 0)
-        return (NULL);
-    arr = malloc(sizeof(char) * size);
-    if (arr == NULL)
-	return (NULL);
+char *arr;
+unsigned int i;
+if (size == 0)
+return (NULL);
+arr = malloc(sizeof(char) * size);
+if (arr == NULL)
+return (NULL);
 
-    for (i = 0; i < size; i++)
-        arr[i] = c;
-    return (arr);
+for (i = 0; i < size; i++)
+arr[i] = c;
+return (arr);
 }
