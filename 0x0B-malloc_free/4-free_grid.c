3478d3e1d2f847504c0b1db6ad32052c95bf314a
admidere <admasud89@gmail.com>
2022-10-05 00:10:27 +0300

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* free_grid -> removing memory allocation
+* @grid: parameter
+* @height: height parameter
+* Return: nothing
+*/
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
