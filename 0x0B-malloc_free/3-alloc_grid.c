fe0f2f0b3564212c4285863f7bccabbf69c992a7
admidere <admasud89@gmail.com>
2022-10-04 23:12:34 +0300

    2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+* alloc_grid -> string concatinating function
+* @width: width parameter
+* @height: height parameter
+* Return: print 2d array
+*/
+
+int **alloc_grid(int width, int height)
+{
+int i, j, arr2d;
+if (width <= 0 || height <= 0)
+return (NULL);
+arr2d = malloc(width * height * sizeof(int));
+if (arr2d == NULL)
+free(a);
+return (NULL);
+
+for (i = 0; i < width; i++)
+{
+for (j = 0; j < height; j++)
+arr2d[i][j] = 0;
+}
+return (arr2d);
+}
