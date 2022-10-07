df138865df5d2b4edd0434fe74224ca743b017ed
admidere <admasud89@gmail.com>
2022-10-07 17:53:20 +0300

    calloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,17 @@
 /**
 * malloc_checked - function that allocates memory using malloc.
 * @b: the number that allocat memory
-* Return: A pointer to the allocated memory.
+* Return: void
 */
 void *malloc_checked(unsigned int b)
 {
-void *mem = malloc(b);
+void *ptr;
 
-if (mem == NULL)
+ptr = malloc(b);
+
+if (ptr == NULL)
+{
 exit(98);
+}
 
-return (mem);
+return (ptr);
