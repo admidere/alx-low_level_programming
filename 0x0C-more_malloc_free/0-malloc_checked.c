765b356b4bcfe21b4a8be4e4be4e7d6cef2cfd7a
admidere <admasud89@gmail.com>
2022-10-07 17:48:23 +0300

    calloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - function that allocates memory using malloc.
- * @b: the number that allocat memory
- * Return: A pointer to the allocated memory.
- */
+* malloc_checked - function that allocates memory using malloc.
+* @b: the number that allocat memory
+* Return: A pointer to the allocated memory.
+*/
 void *malloc_checked(unsigned int b)
 {
-	void *memory = malloc(b);
+void *mem = malloc(b);
 
-	if (memory == NULL)
-		exit(98);
+if (mem == NULL)
+exit(98);
 
-	return (memory);
+return (mem);
