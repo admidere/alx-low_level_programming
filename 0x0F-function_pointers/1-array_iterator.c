cca263e07de62c38bd7815955b6a30e03f1421a1
admidere <admasud89@gmail.com>
2022-10-14 17:23:13 +0300

    print array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,7 @@
 #include "function_pointers.h"
 
 /**
-* array_iterator - executes a function given as a
-
+* array_iterator - executes a function
 * @array: array.
 * @size: size of the array.
 * @action: pointer
