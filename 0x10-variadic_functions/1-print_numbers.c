d77e29ff8d7d8dce3536434dc47be4c50695ae01
admidere <admasud89@gmail.com>
2022-10-14 11:14:48 +0300

    print the numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,16 +10,20 @@
 * return: sum
 */
 void print_numbers(const char *separator, const unsigned int n, ...)
+va_list nums;
+unsigned int i;
+
+va_start(nums, n);
+
+for (i = 0; i < n; i++)
 {
-	 va_list num;
-	 unsigned int i;
-	 va_start(num, n);
-	 for (i = 0; i < n; i++)
-	 {
-	 printf("%d", va_arg(num, int));
-	 if(i != (n - 1) && separator != null)
-	 printf("%s", separator);
-	 }
-	 printf("\n");
-	 va_end(num);
+printf("%d", va_arg(nums, int));
+
+if (index != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+
+printf("\n");
+
+va_end(nums);
 }
