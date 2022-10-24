8010970c4b21ba59795b7f91a8c2d2a24db4b233
admidere <admasud89@gmail.com>
2022-10-25 00:03:05 +0300

    print the number of listed

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+* print_listint - function that returns
+* the number of elements in a linked listint_t list.
+* @h: head of a list.
+* Return: numbers of nodes.
+*/
+
+size_t listint_len(const listint_t *h)
+{
+size_t newnodes = 0;
+
+while (h != NULL)
+{
+h = h->next;
+newnodes++;
+}
+return (newnodes);
+}
