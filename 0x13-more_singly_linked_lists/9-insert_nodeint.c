b4161235d45eab33d2eab541b8738707f32d5369
admidere <admasud89@gmail.com>
2022-10-25 15:55:50 +0300

    insert the given element

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index - inserts a new node
-* at a given position.
+* insert_nodeint_at_index - function that inserts
+* a new node at a given position.
 * @head: head of a list.
-* @idx: index of the list where the new node is
-* added.
+* @idx: index of the list
+* where the new node is added.
 * @n: integer element.
 *
 * Return: the address of the new node, or NULL if it
@@ -18,7 +18,14 @@ unsigned int i;
 listint_t *ptr2 = malloc(sizeof(listint_t));
 listint_t *ptr = *head;
 ptr2->n = n;
-ptr->link = NULL;
+
+if (idx != 0)
+{
+for (i = 0; i < idx - 1 && ptr != NULL; i++)
+{
+ptr = ptr->next;
+}
+}
 
 if (ptr == NULL && idx != 0)
 return (NULL);
@@ -26,19 +33,16 @@ return (NULL);
 if (ptr2 == NULL)
 return (NULL);
 
-idx--;
-while(idx != 1)
+if (idx == 0)
 {
-ptr = ptr->next;
-idx--;
+ptr2->next = *head;
+*head = ptr2;
 }
+else
+{
 ptr2->next = ptr->next;
 ptr->next = ptr2;
-
-i = 0;
-while (ptr != NULL)
-{
-printf("%d ", ptr->n);
-ptr = ptr->next;
 }
+
+return (ptr2);
 }
