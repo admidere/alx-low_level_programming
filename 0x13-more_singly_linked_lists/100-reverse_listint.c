caea15006a35f4341a863228cdbbf6a5f3aad23f
admidere <admasud89@gmail.com>
2022-10-25 22:20:00 +0300

    reversed

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,17 +10,17 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = NULL;
+listint_t *prev = NULL;
+listint_t *current = NULL;
 
-	while (*head != NULL)
-	{
-		current = *head->next;
-		*head->next = prev;
-		prev = *head;
-		*head = current;
-	}
-	*head = prev;
+while (*head != NULL)
+{
+current = *head->next;
+*head->next = prev;
+prev = *head;
+*head = current;
+}
+*head = prev;
 
-	return (*head);
+return (*head);
 }
