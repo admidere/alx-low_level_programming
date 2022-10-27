3d04fc105241d2579ca57704ce022a069f1b503c
admidere <admasud89@gmail.com>
2022-10-27 18:29:25 +0300

    convert a binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include "main_h"
+#include "main.h"
 
 /**
 * binary_to_uint - function that converts
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 		return (0);
-		decimal = 2 * decimal + (b[i] - '0');
+		decimal = 2 * decimal + (b[i] - 48);
 	}
 
 	return (decimal);
