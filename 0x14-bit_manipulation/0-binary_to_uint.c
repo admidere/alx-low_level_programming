2685c15b00ef799375eba4dcd06f1ee03be98ae0
admidere <admasud89@gmail.com>
2022-10-27 19:09:09 +0300

    convert a binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,26 +10,26 @@
 */
 
 unsigned int binary_to_uint(const char *b)
+
 {
 	unsigned int decimal = 0;
-	int i, base;
+	int base = 1, i = 0;
 
-	if (!b)
-	return (0);
+	if (b == NULL)
+		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
-	            ;
-	
-	for (i--, base = 1; i >= 0; i--, base *= 2)
+	while (b[i + 1])
 	{
 		if (b[i] != '0' && b[i] != '1')
-		{
-		return (0);
-		}
-		if (b[i] & 1)
-		{
-		decimal += base;
-		}
+			return (0);
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		decimal += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
 	}
 
 	return (decimal);
