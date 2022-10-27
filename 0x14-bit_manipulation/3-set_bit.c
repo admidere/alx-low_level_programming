641c78f4789a18b83f42aa5ce07809addbf09992
admidere <admasud89@gmail.com>
2022-10-27 20:40:16 +0300

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 * set_bit - Entry Point
 * @n: input
 * @index: index
 * Return: 1 if success
 */
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int bits;
