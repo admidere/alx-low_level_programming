8c18c3fad8072822e08e4708b113fb586c301466
admidere <admasud89@gmail.com>
2022-10-27 20:12:55 +0300

    entry point

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * get_bit - Entry Point
- * @n: input
- * @index: index
- * Return: 0
- */
- 
+* get_bit - Entry Point
+* @n: input
+* @index: index
+* Return: 0
+*/
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int bits;
