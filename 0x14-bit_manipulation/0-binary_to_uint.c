5ebacac5e246f47d4be089d38a7506402192f840
admidere <admasud89@gmail.com>
2022-10-27 18:30:33 +0300

    convert a binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -21,7 +21,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 		return (0);
-		decimal = 2 * decimal + (b[i] - 48);
+		decimal = 2 * decimal + (b[i] - '0');
 	}
 
 	return (decimal);
