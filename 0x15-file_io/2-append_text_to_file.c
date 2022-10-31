889b2255d029e8d7e524ec836a9b9820ab2207ad
admidere <admasud89@gmail.com>
2022-10-31 20:37:16 +0300

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -5,11 +5,10 @@
 * at the end of a file.
 * @filename: filename.
 * @text_content: added content.
-*
 * Return: 1 if the file exists. 
 * -1 if the fails does not exist
-* or if it fails.
 */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
