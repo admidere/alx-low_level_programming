5af2bcdd0c2b035fa0d23bdf913aa0367373442d
admidere <admasud89@gmail.com>
2022-12-08 21:50:45 +0000

    linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,14 +7,14 @@
  *
  * Return: void
  */
- void free_dlistint(dlistint_t *head)
- {
-     dlistint_t *tmp;
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
 
-     while (head != NULL)
-     {
-         tmp = head->next;
-         free(head);
-         head = tmp;
-     }
- }
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
