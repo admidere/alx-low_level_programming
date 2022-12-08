7228339c2e6fbfc0b0a31176193bf4391936ea77
admidere <admasud89@gmail.com>
2022-12-08 22:58:53 +0000

    linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - finds a specific node of a linked list
+* @head: pointer to the beginning of the list
+* @index: index of the node to retrieve
+*
+* Return: pointer to the indexed node, or NULL on failure
+*/
+int sum_dlistint(dlistint_t *head);
+{
+    int sum = 0;
+    if (head == NULL)
+    return (0);
+    while (head != NULL)
+    {
+        sum += head->n;
+        head = head->next;
+    }
+
+}
