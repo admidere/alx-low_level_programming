e29d2d24a36366bfbe5219922e634e482ba8a9f1
admidere <admasud89@gmail.com>
2022-12-08 22:30:24 +0000

    linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - finds a specific node of a linked list
+* @head: pointer to the beginning of the list
+* @index: index of the node to retrieve
+*
+* Return: pointer to the indexed node, or NULL on failure
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    dlistint_t *tmp, *tmp2, *newp;
+    if (head == NULL)
+    return (NULL);
+    newp = malloc(sizeof(dlistint_t));
+    if (newp == NULL)
+    return (NULL);
+    newp->next = NULL;
+    tmp = head;
+    while (index != 1)
+    {
+        tmp = tmp->next;
+        index--;
+    }
+    if (tmp->next == NULL)
+    {
+        newp->prev = tmp;
+        tmp->next = newp;
+    }
+    else
+    {
+        tmp2 = tmp->next;
+        tmp->next = newp;
+        newp->prev = tmp;
+        tmp2->prev = newp;
+        newp->next = tmp2;
+    }
+    return (head);
+}
