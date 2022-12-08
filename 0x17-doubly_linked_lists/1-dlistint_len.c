cd94cbd3c40efe5db3af9396ef60567f21c355f1
admidere <admasud89@gmail.com>
2022-12-08 18:59:31 +0000

    linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *dlistint_len- print the number of linked list
+ *@h: the ptr to doubly linked list
+ *Return: the number of element in the doubly linked list
+ */
+size_t dlistint_len(const dlistint_t *h);
+{
+	size_t count;
+
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (count);
+}
