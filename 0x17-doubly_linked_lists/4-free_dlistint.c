cc5e233880a6d0eeb6cc610a2e43dadebd70e983
admidere <admasud89@gmail.com>
2022-12-08 21:28:40 +0000

    linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *free_dlistint- free memory
+ *@h: the ptr to doubly linked list
+ *Return: the number of element in the doubly linked list
+ */
+ void free_dlistint(dlistint_t *head);
+ {
+     dlistint_t *tmp;
+     while (head != NULL)
+     {
+         tmp = head->next;
+         free(head);
+         head = tmp
+     }
+ }
