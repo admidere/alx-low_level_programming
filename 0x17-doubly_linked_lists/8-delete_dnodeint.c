d39d19993d69eef72323152ec36d57ef450a9053
admidere <admasud89@gmail.com>
2022-12-09 00:08:44 +0000

    linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,39 +1,40 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
-*delete_dnodeint_at_index- unction that deletes the node at index.
-*@head: a ptr to ptr the listint_t list
-*@index:the node that should be deleted. Index starts at 0
-*Return: 1 if it succeeded, -1 if it failed.
+* delete_dnodeint_at_index - deletes a node at a specific index
+* @head: double pointer to the linked list
+* @index: index at which to delete node
+*
+* Return: 1 on success, -1 on failure
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *current;
-dlistint_t *temp;
-unsigned int pos;
+unsigned int i;
 
-if (head == NULL)
-return (-1);
-if (*head == NULL)
+if (head == NULL || *head == NULL)
 return (-1);
 current = *head;
-pos = 0;
 if (index == 0)
 {
-*head = (*head)->next;
+*head = current->next;
+if (current->next != NULL)
+{
+current->next->prev = NULL;
 }
-else
+free(current);
+return (1);
+}
+for (i = 0; i < index; i++)
 {
-while (pos < (index - 1))
-{
-if (current == NULL)
+if (current->next == NULL)
 return (-1);
 current = current->next;
-pos++;
 }
-temp = current;
-current = current->next;
-temp->next = current == NULL ? NULL : current->next;
-}
+current->prev->next = current->next;
+if (current->next != NULL)
+current->next->prev = current->prev;
 free(current);
 return (1);
 }
