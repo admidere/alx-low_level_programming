2e496e81ffd8c8cda60c9437406a7deb7ad8ef5c
admidere <admasud89@gmail.com>
2023-01-06 10:58:41 -0800

    hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     if (!size)
 		return (NULL);
 
-    if (table = NULL)
+    if (!table)
         return (NULL);
 
     table->size = size;
