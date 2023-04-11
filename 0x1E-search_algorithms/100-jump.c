f285f061db6d66dc8e90c086cbcdb5617d7e73b1
admidere <admasud89@gmail.com>
2023-04-11 11:43:35 -0700

    jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -27,7 +27,7 @@ right += step;
 right = (right >= size) ? size - 1 : right;
 
 printf("Value found between indexes [%u] and [%u]\n",
-       (unsigned int)left, (unsigned int)right);
+(unsigned int)left, (unsigned int)right);
 
 while (left <= right)
 {
