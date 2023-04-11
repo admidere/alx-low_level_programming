79d182284ac7ab8a180c63bd089bc1555b9c6b88
admidere <admasud89@gmail.com>
2023-04-11 07:53:12 -0700

    binary search logarithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,37 +11,37 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-        int i;
-        int mid;
-        int left = 0;
-        int right = size - 1;
-        if (array == NULL || size == 0)
-        {
-                return (-1);
-        }
-        while (left <= right)
-        {
-                printf("Searching in array: ");
-                for (i = left; i <= right; i++)
-                {
-                        printf("%d", array[i]);
-                        if (i != right)
-                        printf(", ");
-                }
-                printf("\n");
-                mid = (left + right) / 2;
-                if (array[mid] == value)
-                {
-                        return (mid);
-                }
-                else if (array[mid] > value)
-                {
-                        right = mid - 1;
-                }
-                else
-                {
-                        left = mid + 1;
-                }
-        }
-        return (-1);
+int i;
+int mid;
+int left = 0;
+int right = size - 1;
+if (array == NULL || size == 0)
+{
+return (-1);
+}
+while (left <= right)
+{
+printf("Searching in array: ");
+for (i = left; i <= right; i++)
+{
+printf("%d", array[i]);
+if (i != right)
+printf(", ");
+}
+printf("\n");
+mid = (left + right) / 2;
+if (array[mid] == value)
+{
+return (mid);
+}
+else if (array[mid] > value)
+{
+right = mid - 1;
+}
+else
+{
+left = mid + 1;
+}
+}
+return (-1);
 }
