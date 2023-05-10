24c5164b59afe6a6557ff9ce1a930f0e2c78388c
admidere <admasud89@gmail.com>
2023-05-10 13:27:17 -0700

    binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -38,7 +38,7 @@ int binary_search(int *array, size_t size, int value)
                 }
                 if (array[mid] == value)
                 {
-                        return mid;
+                        return (mid);
                 }
                 else if (array[mid] < value)
                 {
