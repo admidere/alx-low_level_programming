47f151bb68db2de4ff2284ca2fdffd923bca7318
admidere <admasud89@gmail.com>
2023-05-10 13:32:17 -0700

    binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search -  value in a sorted array of integers
+ * binary_search - value in a sorted array of integers
  * @array: pointer to the first element of the array
  * @size: size of an array
  * @value: value to search for
