2ce8319d47f1eec8aacb2b363c18008afc4a4ba9
admidere <admasud89@gmail.com>
2023-07-12 09:31:48 -0700

    binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,6 +16,9 @@ int binary_search(int *array, size_t size, int value)
 	size_t right = size - 1;
 	size_t i, mid;
 
+	if (!array)
+		return(-1);
+
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
