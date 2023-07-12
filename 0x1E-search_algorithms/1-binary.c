35854f644e5bacfb446f4d05db525d0b320f99bc
admidere <admasud89@gmail.com>
2023-07-12 09:28:02 -0700

    binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -21,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
 		{
-			printf("%lu", i);
+			printf("%d", array[i]);
 			if (i != right)
 			{
 				printf(", ");
