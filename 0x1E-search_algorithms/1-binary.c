e0bef78b83d8e19214da625e3b469449c151e9fc
admidere <admasud89@gmail.com>
2023-07-12 04:36:52 -0700

    binary searech algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - function which is looking for desired valued in aan array
+ * using binary search algorithm
+ * @array: array which is pointer to first index
+ * @size: size of array
+ * @value: desired value to search in an array
+ * Return: the value of index if it is found, -1 otherwise
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t i, mid;
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%lu", i);
+			if (i != right)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+
+		if (value == array[mid])
+		{
+			return (mid);
+		}
+		else if (value > array[mid])
+		{
+			left = mid + 1;
+		}
+		else
+			right = mid - 1;
+	}
+	return (-1);
+}
